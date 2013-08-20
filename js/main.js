1167ccb272607b6abc36b75cea7a46f2adcd92aa
Shahal Tharique <shahalpk@gmail.com>
2013-08-20 18:53:52 +0530

    added map, now customize it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+// 26.291679,50.210517
+
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(26.291679,50.210517),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
